24f3d98c83e739f11b908a9f07bd371bebdb2598
Kishan <bangsikishan@duck.com>
2023-08-22 17:50:08 +0545

    Added try except block

--- utils/bid_info_extractor.py
+++ utils/bid_info_extractor.py
@@ -1,13 +1,17 @@
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 
 def extract_bid_info(child_element, bid_no_xpath: str, title_xpath: str, due_date_xpath: str, index: int) -> tuple:
-    bid_no_element = child_element.find_element(By.XPATH, bid_no_xpath.format(index=index))
-    bid_no = bid_no_element.text[:20] or bid_no_element.get_attribute("textContent")[:20]
-    
-    title_element = child_element.find_element(By.XPATH, title_xpath.format(index=index))
-    title = title_element.text[:40] or title_element.get_attribute("textContent")[:40]
-    
-    due_date_element = child_element.find_element(By.XPATH, due_date_xpath.format(index=index))
-    due_date = due_date_element.text or due_date_element.get_attribute("textContent")
+    try:
+        bid_no_element = child_element.find_element(By.XPATH, bid_no_xpath.format(index=index))
+        bid_no = bid_no_element.text[:20] or bid_no_element.get_attribute("textContent")[:20]
+        
+        title_element = child_element.find_element(By.XPATH, title_xpath.format(index=index))
+        title = title_element.text[:40] or title_element.get_attribute("textContent")[:40]
+        
+        due_date_element = child_element.find_element(By.XPATH, due_date_xpath.format(index=index))
+        due_date = due_date_element.text or due_date_element.get_attribute("textContent")
 
-    return bid_no, title, due_date
+        return bid_no, title, due_date
+    except NoSuchElementException:
+        pass
