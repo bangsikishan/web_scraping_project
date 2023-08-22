23cd81f91ac0b5e268821a8cba75a42345d6c1e5
Kishan <bangsikishan@duck.com>
2023-08-22 17:52:27 +0545

    Added extra check to prevent adding mailto links

--- utils/link_filter.py
+++ utils/link_filter.py
@@ -2,7 +2,7 @@ def filter_unique_links(link_elements: list) -> list:
     filtered_links = []
 
     for link_element in link_elements:
-        if (link := link_element.get_attribute("href")) not in filtered_links:
+        if (link := link_element.get_attribute("href")) not in filtered_links and link is not None and "mailto" not in link:
             filtered_links.append(link)
 
     return filtered_links
